2fcea34410a125f1039d02d9ca9d8b2b530fd4f4
Daz Grant <dagrant@redhat.com>
2022-07-08 08:37:21 -0400

    Added json logging, improved BuildPush

--- simple-bash/src/BuildPush.sh
+++ simple-bash/src/BuildPush.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# Uncomment and set the following environment variables.
+read -p "REGISTRY [quay.io]:       " REGISTRY    # e.g. quay.io
+read -p "USERNAME [none]:          " USERNAME    
+read -p "REPOSITORY [simple-bash]: " REPOSITORY  # e.g. simple-bash
+read -p "TAG [latest]:             " TAG         # e.g. 1.4 
+read -p "Push to latest? [n]:      " PUSH_TO_LATEST
 
-#REGISTRY="<registry eg. quay.io>"
-#USERNAME="<registry username>"
-#REPOSITORY="simple-bash"
-#TAG="1.0"
+REGISTRY=${REGISTRY:-quay.io}
+USERNAME=${USERNAME:-none}
+REPOSITORY=${REPOSITORY:-simple-bash}
+TAG=${TAG:-latest}
+PUSH_TO_LATEST=${PUSH_TO_LATEST:-N} 
+PUSH_TO_LATEST=${PUSH_TO_LATEST^^}
 
-[[ -z "$REGISTRY" ]] && echo "REGISTRY is not defined!" && exit 1
-[[ -z "$USERNAME" ]] && echo "REGISTRY is not defined!" && exit 1
-[[ -z "$REPOSITORY" ]] && echo "REGISTRY is not defined!" && exit 1
-[[ -z "$TAG" ]] && echo "REGISTRY is not defined!" && exit 1
-
-echo -e "\n\nBuilding & pushing: $REGISTRY/$USERNAME/$REPOSITORY:$TAG\n\n"
+echo -e "\n\nBuilding & pushing: $REGISTRY/$USERNAME/$REPOSITORY:$TAG"
+echo -e "Pushing to latest?: $PUSH_TO_LATEST\n\n"
 
 podman build . --format=docker -t $REGISTRY/$USERNAME/$REPOSITORY:$TAG
 podman push $REGISTRY/$USERNAME/$REPOSITORY:$TAG
 
-podman tag $REGISTRY/$USERNAME/$REPOSITORY:$TAG $REGISTRY/$USERNAME/$REPOSITORY:latest
-podman push $REGISTRY/$USERNAME/$REPOSITORY:latest
-
-
+# Already "latest" - No need to tag and push" 
+if [[ "$TAG" == "latest" ]]; then
+  echo "Skipping, tag has already been pushed as latest ..."
+else
+  if [[ "$PUSH_TO_LATEST" == "Y" ]]; then
+    echo -e "\n\nTagging & pushing $TAG to latest: $REGISTRY/$USERNAME/$REPOSITORY:latest"
+    podman tag $REGISTRY/$USERNAME/$REPOSITORY:$TAG $REGISTRY/$USERNAME/$REPOSITORY:latest
+    podman push $REGISTRY/$USERNAME/$REPOSITORY:latest
+  fi
+fi
--- simple-bash/src/app.sh
+++ simple-bash/src/app.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-DB="/apps/data"
-VERSION="1.1"
+# Description: Simple Bash script to generate audit-like test data.
+# Version: 1.2
+# Date: 9-Jul-2022
+# Author: Daz Grant (dagrant@redhat.com)
 
-echo -e "Script Path: $(readlink -f $0)\nVersion: $VERSION"
+DB="/apps/data"
+VERSION="1.2"
+AUDIT_LOG_DIR="/apps/logs"
+
+[[ ! -d $AUDIT_LOG_DIR ]] && mkdir $AUDIT_LOG_DIR
+
+log() {
+  local TIMESTAMP="$(date +%s)"
+  local MSG="$1"
+  local SEVERITY="$2"
+
+  [ -z "$MSG" ] && MSG="<empty>"
+  [ -z "$SEVERITY" ] && SEVERITY="INFO"
+
+  JSON_LOG_STRING="{\"log-event\":{\"timestamp\":\"$TIMESTAMP\",\"host\":\"$HOSTNAME\",\"pid\":\"$$\",\"message\":\"$MSG\",\"severity\":\"$SEVERITY\"}}}"
+  echo $JSON_LOG_STRING
+}
+
+VERSION_INFO="Script Path: $(readlink -f $0) Version: $VERSION"
+
+log "Starting" INFO
+log "$VERSION_INFO" INFO
 
 while true
 do
+  TIMESTAMP="$(date +%s)"
   FIRST_NAME_LIST=$(echo -e "male-given-names.txt\nfemale-given-names.txt" | shuf -n1)
   FIRST_NAME=$(shuf -n1 $DB/$FIRST_NAME_LIST)
   LAST_NAME=$(shuf -n1 $DB/surnames.txt)
   AUDIT_EVENT=$(shuf -n1 $DB/events.txt)
   AUDIT_STATUS=$(echo -e "Success\nFailure" | shuf -n1)
-  LOG_STRING="Timestamp=\"$(date +%s)\" Host=\"$HOSTNAME\" pid=\"$$\" Event=\"$AUDIT_EVENT\" Status=\"$AUDIT_STATUS\" User=\"$FIRST_NAME $LAST_NAME\""
-  echo "$LOG_STRING"
+  LOG_STRING="Timestamp=\"$TIMESTAMP\" Host=\"$HOSTNAME\" pid=\"$$\" Event=\"$AUDIT_EVENT\" Status=\"$AUDIT_STATUS\" User=\"$FIRST_NAME $LAST_NAME\""
+  JSON_LOG_STRING="{\"audit-event\":{\"timestamp\":\"$TIMESTAMP\",\"host\":\"$HOSTNAME\",\"pid\":\"$$\",\"event\":\"$AUDIT_EVENT\",\"status\":\"$AUDIT_STATUS\",\"user\":{\"firstname\":\"$FIRST_NAME\",\"surname\":\"$LAST_NAME\"}}}"
+  echo $JSON_LOG_STRING | jq -c . | tee -a $AUDIT_LOG_DIR/audit-event-log.json
   sleep 5
 done
